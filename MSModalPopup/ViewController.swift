131d088d1487dc92cc0403680cd0795a607cd6ba
Mansoor Said <mansoor-shah-said@hotmail.com>
2017-12-24 17:02:29 -0500

    Initial Commit

---
+++ MSModalPopup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSModalPopup
+//
+//  Created by Mansoor Shah Said on 2017-12-24.
+//  Copyright © 2017 Mansoor Shah Said. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
